b3144809322bede8828858355483c7874f20eb50
Sai Mounika <saimounikanedunuri@gmail.com>
2024-01-08 22:24:52 +0530

    Delete HackerRank/unique.sql

--- HackerRank/unique.sql
+++
@@ -1,5 +0,0 @@
-Query:
-Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
-Code:
-select distinct city from station 
-where id%2=0;
