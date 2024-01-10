da5ac0a461fd35d4eb5a002fe02af29118639d49
Sai Mounika <saimounikanedunuri@gmail.com>
2024-01-10 13:30:25 +0530

    aggregation.sql

---
+++ HackerRank/Day10/aggregation.sql
@@ -0,0 +1,11 @@
+Query:
+Consider  and  to be two points on a 2D plane.
+
+ happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+ happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+ happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+ happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
+
+Code:
+select round((max(lat_n)-min(lat_n))+(max(long_w)-min(long_w)),4) from station;
