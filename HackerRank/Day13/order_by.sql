90c1f35c23f971d28a895e09d534f615381eb6a6
Sai Mounika <saimounikanedunuri@gmail.com>
2024-01-13 22:32:35 +0530

    order_by.sql

---
+++ HackerRank/Day13/order_by.sql
@@ -0,0 +1,5 @@
+Query:
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+Code:
+select name from employee
+order by name;
