da3690bceb563615e6e1d3b0a6c5b9fdc2b744da
Sai Mounika <saimounikanedunuri@gmail.com>
2024-01-14 14:49:34 +0530

    reverse_array.py

---
+++ HackerRank/Day14/reverse_array.py
@@ -0,0 +1,13 @@
+Question:
+Reverse an array
+Python Code:
+method-1:
+res=(a[::-1])
+result = ''.join(map(str, res))
+
+method-2:
+print(reverseArray(a))
+
+method-3:
+a.reverse()
+a=list(reversed(a))
